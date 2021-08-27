fcfffd346c8df33d29c5b6c62468bd6383b87891
speedmaster24 <speedmaster2424@gmail.com>
2021-08-27 10:03:52 -0600

    functions assigment completed

--- Student/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Student/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -7,8 +7,23 @@
 
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
-
-
+let randomNum = Int.random(in: 0...4)
+func eightball() {
+    switch randomNum {
+    case 1:
+        print("Yes")
+    case 2:
+        print("NO")
+    case 3:
+        print("Ask again later")
+    case 4:
+        print("Make your own path")
+    default:
+        print("You're eight ball has rejected god and decided to destroy the universe. my how you have screwed up")
+    }
+    
+}
+eightball()
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
  */
--- Student/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ Student/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -9,11 +9,36 @@
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
-
-
+func progressUpdate(_ steps: Double, _ goal: Double) {
+    if steps==0 {
+        print("Get moving!")
+    } else if steps<(goal*0.1) {
+        print("You're off to a good start.")
+    } else if steps<(goal*0.5) {
+        print("You're almost halfway there!")
+    } else if steps<(goal*0.9) {
+        print("You're over halfway there!")
+    } else if steps<goal {
+        print("You're almost there!")
+    } else {
+        print("You beat your goal!")
+    }
+}
+progressUpdate(0, 10000)
+progressUpdate(100, 10000)
+progressUpdate(1000, 10000)
+progressUpdate(6000, 10000)
+progressUpdate(9999, 10000)
+progressUpdate(10000, 10000)
 //:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
-
-
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    if currentDistance/totalDistance<=currentTime/goalTime {
+        print("Keep it up!")
+    } else {
+        print("You've got to push it just a bit harder!")
+    }
+}
+pacing(currentDistance: 0.5, totalDistance: 1, currentTime: 5, goalTime: 10)
 /*:
 [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
  */
