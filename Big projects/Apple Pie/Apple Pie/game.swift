400c3ddd69e862ecddbfe7488a82b6e39a3939a6
speedmaster24 <speedmaster2424@gmail.com>
2021-10-08 10:12:31 -0600

    finished apple pie
    i finished the apple pie project and am now going to attempt to randomize the words that are picked

---
+++ Big projects/Apple Pie/Apple Pie/game.swift
@@ -0,0 +1,34 @@
+//
+//  game.swift
+//  Apple Pie
+//
+//  Created by speed Master on 10/8/21.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+
+
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
