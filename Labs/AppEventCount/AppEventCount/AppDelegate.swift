b2c1c51b8b1e89d924e5e25cc19067edc6d8ae70
speedmaster24 <speedmaster2424@gmail.com>
2021-10-18 08:37:32 -0600

    appeventcount fully finished finaly

--- Labs/AppEventCount/AppEventCount/AppDelegate.swift
+++ Labs/AppEventCount/AppEventCount/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        launchCount += 1
         return true
     }
 
@@ -23,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        configurationForConnectingCount += 1
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
--- Labs/AppEventCount/AppEventCount/ViewController.swift
+++ Labs/AppEventCount/AppEventCount/ViewController.swift
@@ -24,15 +24,23 @@ class ViewController: UIViewController {
     var sceneDidEnterBackgroundFinaly = 0
     
     var appDelegate = (UIApplication.shared.delegate as! AppDelegate)
-    func updateView() {
-        applicationfinishedlaunching.text = "The App has launched \(appDelegate.launchCount) time(s)"
-    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateView()
         // Do any additional setup after loading the view.
     }
 
+    func updateView() {
+        applicationfinishedlaunching.text = "The App has launched \(appDelegate.launchCount) time(s)"
+        applicationConfigured.text = "The App configured \(appDelegate.configurationForConnectingCount) time(s)"
+        sceneWillConnect.text = "SceneWillConnect \(sceneWillConnectFinaly) time(s)"
+        sceneDidBecomeActive.text = "SceneDidBecomeActive \(sceneDidBecomeActiveFinaly) time(s)"
+        SceneWillResignActive.text = "SceneWillResignActive \(sceneWillResignActiveFinaly) time(s)"
+        sceneWillEnterForground.text = "SceneWillEnterForground \(sceneWillEnterForgroundFinaly) time(s)"
+        sceneDidEnterBackground.text = "SceneDidEnterBackground \(sceneDidEnterBackgroundFinaly) time(s)"
+    }
 
 }
 
